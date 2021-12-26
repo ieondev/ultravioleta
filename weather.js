c0fb94d1c68cda28ba3ea2727a65666b33813e4b
deathflaresm <61573687+deathflaresm@users.noreply.github.com>
2021-12-26 15:08:10 -0600

    Delete weather.js

--- weather.js
+++
@@ -1,9 +0,0 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=25&lon=-100').then(function (response) {
-    return response.json();
-})
-    .then(function (myJson) {
-        document.getElementById('t').innerHTML = myJson.weather[0].description;
-        var img = document.createElement("img");
-        img.src = 'http://openweathermap.org/img/w/'+ myJson.weather[0].icon +'.png';
-        document.getElementById('u').appendChild(img);
-    })
