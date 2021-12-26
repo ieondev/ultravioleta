34ec2bf8fc5d5be779f25d298d8804a15f8b3c1d
deathflaresm <61573687+deathflaresm@users.noreply.github.com>
2021-12-26 15:07:49 -0600

    Delete bitcoin.js

--- bitcoin.js
+++
@@ -1,5 +0,0 @@
-fetch('https://api.coindesk.com/v1/bpi/currentprice.json').then(function (response) {
-    return response.json();
-})
-    .then(function (myJson) {
-        document.getElementById('s').innerHTML = "BTC $" + myJson.bpi.USD.rate_float.toFixed(2);})
